d45647af21586d86709f0ee89123f6c4e19d0d2b
HICHAM IAMIRI <89263522+HichamDe@users.noreply.github.com>
2024-02-09 13:55:13 +0100

    adding touchable opacity

--- App.js
+++ App.js
@@ -1,31 +1,27 @@
-import React, { useState, useEffect } from 'react'
-import { Image } from 'react-native'
+import { View, Text, Image, TouchableOpacity } from 'react-native'
+import React from 'react'
 import DocumentScanner from 'react-native-document-scanner-plugin'
 
-export default function App (){
-  const [scannedImage, setScannedImage] = useState();
-
-  const scanDocument = async () => {
-    // start the document scanner
-    const { scannedImages } = await DocumentScanner.scanDocument()
-  
-    // get back an array with scanned image file paths
-    if (scannedImages.length > 0) {
-      // set the img src, so we can view the first scanned image
-      setScannedImage(scannedImages[0])
-    }
-  }
-
-  useEffect(() => {
-    // call scanDocument on load
-    scanDocument()
-  }, []);
-
+const App = () => {
   return (
-    <Image
-      resizeMode="contain"
-      style={{ width: '100%', height: '100%' }}
-      source={{ uri: scannedImage }}
-    />
+    <View style={{ flex: 1, borderWidth:1,borderColor:"red",}}>
+
+      <TouchableOpacity
+        style={{
+          width: 70,
+          height: 70,
+          borderRadius: 35,
+          backgroundColor: "black",
+          justifyContent: "center",
+          alignItems: "center",
+          position: "absolute",
+          bottom: 20,
+          alignSelf:"center"
+        }}>
+        <Text style={{ color: "white" }}>Scan</Text>
+      </TouchableOpacity>
+    </View>
   )
 }
+
+export default App

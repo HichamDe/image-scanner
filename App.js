ad70fd9de9cae0d79d2cd711d6375001cf8c50dd
HICHAM IAMIRI <89263522+HichamDe@users.noreply.github.com>
2024-02-09 21:16:11 +0100

    image path undefined

--- App.js
+++ App.js
@@ -1,12 +1,28 @@
 import { View, Text, Image, TouchableOpacity } from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import DocumentScanner from 'react-native-document-scanner-plugin'
 
 const App = () => {
-  return (
-    <View style={{ flex: 1, borderWidth:1,borderColor:"red",}}>
+  const [scannedImage, setScannedImage] = useState();
 
+  const scanDocument = async () => {
+    console.log("I'm working ")
+    // start the document scanner
+    const { scannedImages } = await DocumentScanner.scanDocument()
+
+    // get back an array with scanned image file paths
+    if (scannedImages.length > 0) {
+      // set the img src, so we can view the first scanned image
+      console.log(scannedImages[0])
+      setScannedImage(scannedImages[0])
+    }
+  }
+
+  return (
+    <View style={{ flex: 1, borderWidth: 1, borderColor: "red", }}>
+      {scannedImage ? <Image source={{ url: scannedImage }} style={{ height: "50%", width: "100%" }} /> : ""}
       <TouchableOpacity
+        onPress={scanDocument}
         style={{
           width: 70,
           height: 70,
@@ -16,7 +32,7 @@ const App = () => {
           alignItems: "center",
           position: "absolute",
           bottom: 20,
-          alignSelf:"center"
+          alignSelf: "center"
         }}>
         <Text style={{ color: "white" }}>Scan</Text>
       </TouchableOpacity>

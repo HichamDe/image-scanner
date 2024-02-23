b3925f8dafbc4c4cae60595305850bb8b84addae
HICHAM IAMIRI <89263522+HichamDe@users.noreply.github.com>
2024-02-23 13:11:32 +0100

    working version

--- App.js
+++ App.js
@@ -12,13 +12,13 @@ export default () => {
     // get back an array with scanned image file paths
     if (scannedImages.length > 0) {
       // set the img src, so we can view the first scanned image
-      setScannedImage("file:///storage/emulated/0/Android/data/com.main/files/Pictures/DOCUMENT_SCAN_0_20240223_1202191817947484524169529.jpg")
+      setScannedImage(scannedImages[0])
     }
   }
 
   return (
     <View style={{ flex: 1, borderWidth: 1, borderColor: "red", }}>
-      <Image source={{ uri: "file:///storage/emulated/0/Android/data/com.main/files/Pictures/DOCUMENT_SCAN_0_20240223_1202191817947484524169529.jpg" }} style={{ height: "50%", borderWidth: 1, width: "100%", borderColor: "white" }} />
+      <Image source={{ uri: scannedImage }} style={{ height: "50%", borderWidth: 1, width: "100%", borderColor: "white" }} />
       <TouchableOpacity
         onPress={scanDocument}
         style={{

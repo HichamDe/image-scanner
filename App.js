88efd4546b0a28114a02bc4066f8fc8381f83bf5
HICHAM IAMIRI <89263522+HichamDe@users.noreply.github.com>
2024-02-23 13:05:16 +0100

    testing Image Component

--- App.js
+++ App.js
@@ -1,26 +1,24 @@
+import React, { useState, useEffect } from 'react'
 import { View, Text, Image, TouchableOpacity } from 'react-native'
-import React, { useEffect, useState } from 'react'
 import DocumentScanner from 'react-native-document-scanner-plugin'
 
-const App = () => {
-  const [scannedImage, setScannedImage] = useState();
+export default () => {
+  const [scannedImage, setScannedImage] = useState("");
 
   const scanDocument = async () => {
-    console.log("I'm working ")
     // start the document scanner
-    const { scannedImages } = await DocumentScanner.scanDocument()
+    const { scannedImages } = await DocumentScanner.scanDocument({})
 
     // get back an array with scanned image file paths
     if (scannedImages.length > 0) {
       // set the img src, so we can view the first scanned image
-      console.log(scannedImages[0])
-      setScannedImage(scannedImages[0])
+      setScannedImage("file:///storage/emulated/0/Android/data/com.main/files/Pictures/DOCUMENT_SCAN_0_20240223_1202191817947484524169529.jpg")
     }
   }
 
   return (
     <View style={{ flex: 1, borderWidth: 1, borderColor: "red", }}>
-      {scannedImage ? <Image source={{ url: scannedImage }} style={{ height: "50%", width: "100%" }} /> : ""}
+      <Image source={{ uri: "file:///storage/emulated/0/Android/data/com.main/files/Pictures/DOCUMENT_SCAN_0_20240223_1202191817947484524169529.jpg" }} style={{ height: "50%", borderWidth: 1, width: "100%", borderColor: "white" }} />
       <TouchableOpacity
         onPress={scanDocument}
         style={{
@@ -39,5 +37,3 @@ const App = () => {
     </View>
   )
 }
-
-export default App

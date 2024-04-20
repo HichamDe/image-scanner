35de053ea67674bca4a962c27570d3c39e1672e2
HICHAM IAMIRI <89263522+HichamDe@users.noreply.github.com>
2024-04-20 10:37:01 +0100

    working cropper

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react'
-import Svg, { Path, Circle, Rect, ClipPath, Defs, Mask } from 'react-native-svg';
-import { MaskedView } from '@react-native-masked-view/masked-view';
+import React, { useState, useEffect, useLayoutEffect } from 'react'
+import Svg, { Path, Circle, Rect, ClipPath, Defs, Mask, Image as SvgImage } from 'react-native-svg';
+import MaskedView from '@react-native-masked-view/masked-view';
 import { View, Image, PanResponder } from 'react-native'
 
 export default () => {
@@ -26,27 +26,28 @@ export default () => {
           y: newPoints[index].y + dy,
         };
         setPoints(newPoints);
-        setClippingPath(`M${newPoints[0].x},${newPoints[0].y} L${newPoints[1].x},${newPoints[1].y} L${newPoints[2].x},${newPoints[2].y} L${newPoints[3].x},${newPoints[3].y} Z`);
 
       },
     });
 
+  useLayoutEffect(() => {
+    setClippingPath(`M${points[0].x},${points[0].y} L${points[1].x},${points[1].y} L${points[2].x},${points[2].y} L${points[3].x},${points[3].y} Z`);
+  }, [points]);
 
   return (
     <View style={{ flex: 1, borderColor: "white", borderWidth: 1 }}>
-
-      <MaskedView
-        style={{ flex: 1 }}
-        maskElement="test">
-        <Image
-          source={{ uri: "https://png.pngtree.com/png-clipart/20220829/ourmid/pngtree-indoor-plant-png-image_6129530.png" }}
-          style={{ width: '100%', height: '100%' }}
-          resizeMode="cover"
+      <Svg width="100%" height="100%">
+        <Defs key={clippingPath}>
+          <ClipPath id="myMask">
+            <Path d={clippingPath} fill="black" />
+          </ClipPath>
+        </Defs>
+        <SvgImage
+          href={{ uri: "https://png.pngtree.com/png-clipart/20220829/ourmid/pngtree-indoor-plant-png-image_6129530.png" }}
+          width="100%"
+          height="100%"
+          clipPath="url(#myMask)"
         />
-      </MaskedView>
-
-
-      <Svg width="100%" height="100%" style={{ position: 'absolute' }}>
         <Path d={clippingPath} fill="none" stroke="gray" />
         {points.map((point, index) => (
           <Circle
@@ -58,7 +59,6 @@ export default () => {
             {...handleCirclePanResponder(index).panHandlers}
           />
         ))}
-
       </Svg>
     </View>
   )
